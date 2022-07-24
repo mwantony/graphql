f6999eb9aab995660b83469821e8bbfa731a332c
Antony Maziero Warken <againplayi7@gmail.com>
2022-07-24 15:11:52 -0300

    cap 3 vid 1 finalizado

--- api/index.js
+++ api/index.js
@@ -1,12 +1,16 @@
 const { ApolloServer } = require("apollo-server");
 const userSchema = require("./user/schema/user.graphql");
-const userResolvers = require('./user/resolvers/userResolvers')
+const userResolvers = require("./user/resolvers/userResolvers");
+const UsersAPI = require("./user/datasource/user");
 
+const typeDefs = [userSchema];
+const resolvers = [userResolvers];
+const server = new ApolloServer({ typeDefs, resolvers, dataSources: () => {
+  return {
+    usersApi: new UsersAPI()
+  }
+} });
 
-const typeDefs = [userSchema]
-const resolvers = [userResolvers]
-const server = new ApolloServer({ typeDefs, resolvers });
-
-server.listen().then(({url}) => {
-  console.log(`Servidor rodando na url ${url}`)
-})
+server.listen().then(({ url }) => {
+  console.log(`Servidor rodando na url ${url}`);
+});
