f222dec67463a9f59885a74556ec22327de69b2a
Antony Maziero Warken <againplayi7@gmail.com>
2022-07-24 14:37:36 -0300

    cap 2 vid 2 finalizado

--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
 const { ApolloServer } = require("apollo-server");
 const userSchema = require("./user/schema/user.graphql");
-
 const users = [
   {
     nome: "Ana",
@@ -11,8 +10,12 @@ const users = [
     ativo: false,
   },
 ];
+const typeDefs = require([userSchema])
 
-const typeDefs = [userSchema];
 const resolvers = {};
 
 const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({url}) => {
+  console.log(`Servidor rodando na url ${url}`)
+})
