3070c94874e6fe1e9da14e5ad3f6c64f51c2f172
Antony Maziero Warken <againplayi7@gmail.com>
2022-07-24 15:29:04 -0300

    cap 3 vid 2 finalizado

--- api/user/resolvers/userResolvers.js
+++ api/user/resolvers/userResolvers.js
@@ -1,20 +1,8 @@
-const arrayUsers = [
-  {
-    nome: "Ana",
-    ativo: true,
-  },
-  {
-    nome: "Marcia",
-    ativo: false,
-  },
-];
+
 const userResolvers = {
   Query: {
-    users: () => {
-      return arrayUsers
-    },
-    primeiroUser: () => {
-      return arrayUsers[0]
+    users: (root, args, {dataSources}) => {
+      return dataSources.usersAPI.getUsers()
     }
   }
 };
