47fc2639688970ed0f3f45411b32d118cf3161bf
Antony Maziero Warken <againplayi7@gmail.com>
2022-07-24 13:58:37 -0300

    cap 1 finalizado

--- api/index.js
+++ api/index.js
@@ -1,3 +1,24 @@
-const { ApolloServer } = require('apollo-server')
+const { ApolloServer, gql } = require("apollo-server");
 
-const server = new ApolloServer()
+const users = [
+  {
+    nome: "Ana",
+    ativo: true,
+  },
+  {
+    nome: "Marcia",
+    ativo: false,
+  },
+];
+
+const typeDefs = gql`
+  type Users {
+    nome: String!,
+    ativo: Boolean!,
+    email: String
+  }
+
+`
+
+
+const server = new ApolloServer({ typeDefs, resolvers });
